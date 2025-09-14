e9869549fb86b216388d865d24807762919f48ad
Jose Cabrera <jcabrera9409@gmail.com>
2025-09-14 16:56:40 -0500

    adding models

--- task-manager-frontend/src/app/_model/dto.ts
+++ task-manager-frontend/src/app/_model/dto.ts
@@ -1,6 +1,27 @@
-export class ConfirmDataDTO {
-    title: string
-    message: string
-    confirmText: string
-    cancelText: string
+export interface APIResponseDTO<T> {
+  success: boolean;
+  message: string;
+  data: T;
+  statusCode: number;
+  timestamp: string;
+}
+
+export interface AuthenticationResponseDTO {
+  access_token: string;
+  refresh_token: string;
+}
+
+export interface PageableResponseDTO<T> {
+  content: T[];
+  number: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+export interface ConfirmDataDTO {
+    title: string;
+    message: string;
+    confirmText: string;
+    cancelText: string;
 }
